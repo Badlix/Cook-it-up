f1ce059f07a3c0b057c03c5b596b3ac5b9ea33a4
GeniusTom <tomeven13@gmail.com>
2025-01-31 18:19:30 +0100

    Ajout de la condition de de verificaiton de compte

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isVerified()) {
+            throw new CustomUserMessageAccountStatusException('Votre compte n\'a pas encore été vérifié.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+        // Pas besoin de vérifications supplémentaires après authentification
+    }
+}
