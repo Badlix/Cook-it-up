b0bfe14584ddf8cdffdf3e5394088ecfb6e483cb
Badlix <mathieu.leroux173@gmail.com>
2025-02-03 15:20:46 +0100

    add recipes table + fixtures

---
+++ src/Repository/RecipeRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Recipe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Recipe>
+ */
+class RecipeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Recipe::class);
+    }
+
+    /**
+     * @return Recipe[] Returns an array of Recipe objects
+     */
+    public function getAllRecipes(): array
+    {
+        return $this->createQueryBuilder('recipe')
+            ->orderBy('recipe.id', 'ASC')
+            ->setMaxResults(15)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    //    public function findOneBySomeField($value): ?Recipe
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
